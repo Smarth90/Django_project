38fd021e312315fe3364439620963174fffe807c
unknown <samhbk2005@gmail.com>
2026-08-01 10:51:36 +0530

    1

--- tutProject/todos/urls.py
+++ tutProject/todos/urls.py
@@ -8,8 +8,9 @@ urlpatterns = [
     path('hellopy', views.hello_python_view, name = 'hello_python_view'),
     path('', views.render_hello_world, name = 'render_hello_world'),
     path('helloquery', views.hello_query, name = 'helloQuery'),
-    path('postendpoint', views.post_example, name = 'postend'),
+    path('postend', views.post_example, name = 'postend'),
     path('submitForm', views.submit_example, name = 'submitForm'),
+    path('submitdjango', views.submit_django_form, name = 'submitDjango'),
     path('add/<int:num1>/<int:num2>', views.hello_path, name = 'add'),
     path('redirectthis', views.special_view, name='redirectthis')
 ]
